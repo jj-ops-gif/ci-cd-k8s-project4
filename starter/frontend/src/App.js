7ee3493b2f2f841fd58e6842090d7822a05862cf
John Jone <jj20211231@gmail.com>
2024-06-22 14:22:48 -0500

    Add component

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -4,24 +4,24 @@ import MovieDetails from './components/MovieDetails';
 import './App.css';
 
 export default function App() {
-    const [selectedMovie, setSelectedMovie] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-    const handleMovieClick = (movie) => {
-        setSelectedMovie(movie);
-    };
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+  };
 
-    return (
-        <div className="container">
-            <h1>Movie List</h1>
+  return (
+    <div className="container">
+      <h1>Movie List</h1>
 
-            <MovieList onMovieClick={handleMovieClick} />
+      <MovieList onMovieClick={handleMovieClick} />
 
-            {selectedMovie && (
-                <>
-                    <h1>Movie Details</h1>
-                    <MovieDetails movie={selectedMovie} />
-                </>
-            )}
-        </div>
-    );
+      {selectedMovie && (
+        <>
+          <h1>Movie Details</h1>
+          <MovieDetails movie={selectedMovie} />
+        </>
+      )}
+    </div>
+  );
 }
